38629dba9c4405772a1605f05d6e2d2564085761
kinnon <83826176@qq.com>
2018-08-13 15:11:50 +0800

    重写任务<skin:add>：使用stream-series过程中发现并不能保证流程顺序，故不再使用，还需研究

--- plugins/MergeTexture.js
+++ plugins/MergeTexture.js
@@ -2,7 +2,7 @@
  * @Author: Kinnon.Z 
  * @Date: 2018-08-11 17:08:40 
  * @Last Modified by: Kinnon.Z
- * @Last Modified time: 2018-08-11 19:01:21
+ * @Last Modified time: 2018-08-13 11:48:34
  */
 import through from "through2";
 import path from "path";
@@ -14,7 +14,7 @@ module.exports = function(destPath, sheetName) {
     return through.obj(function(file, encode, callback) {
         let p = file.path;
         let o =  path.join(destPath, sheetName);
-        let c = `textureMerger -p ${p} -o ${o} -e /.*.(jpg|png)`;
+        let c = `textureMerger -p ${p} -o ${o} -e /.*.png`;
         exec(c, (err, stdout, stderr) => {
             if (err) {
                 console.log(err);
--- tasks/GenVipRoomSkin.js
+++ tasks/GenVipRoomSkin.js
@@ -2,7 +2,7 @@
  * @Author: Kinnon.Z 
  * @Date: 2018-06-22 10:38:20 
  * @Last Modified by: Kinnon.Z
- * @Last Modified time: 2018-08-11 19:01:30
+ * @Last Modified time: 2018-08-13 15:07:35
  */
 import gulp from "gulp";
 import CONST from "../const";
@@ -25,7 +25,7 @@ import compress from "../plugins/tinify_png_jpg";
 const P = require("gulp-load-plugins")();
 const args = minimist(process.argv.slice(2));
 
-const res_json_path = path.join(CONST.Lrs_Root, CONST.Lrs_Room, "lrsRoom.res.json");
+const lrsResJson = path.join(CONST.Lrs_Root, CONST.Lrs_Room, "lrsRoom.res.json");
 
 /**
  * 判断一张图片是否是合图(通过判断是否有同名.json文件的存在)
@@ -53,48 +53,56 @@ function isSheetPng(file) {
  *     3.拷贝图片资源（icon, preview, 合图及其他jpg散图）,并注入lrsRoom.res.json
  *     4.注入皮肤配置文件vipSkin.json（修改icon, preview, anim, res字段）
  */
-gulp.task("skin:add", () => {
+gulp.task("_skin:move_src", () => {
     let dir = args.dir;
     let name = args.name;
-    let id = args.id;
-    if (!dir || typeof (dir) != "string") {
-        throw new PluginError("skin:added", "YOU MUST SPECIFY A DIRECTORY THAT CONTAINS ALL ASSETS SKIN NEEDED");
-    }
-    if (!name || typeof(name) != "string") {
-        throw new PluginError("skin:added", "YOU MUST SPECIFY A NAME AS NEW FOLDER PLACE SKIN ASSETS");
-    }
-    if (!id) {
-        throw new PluginError("skin:added", "YOU MUST SPECIFY A ID WHICH CONFIG THE ADDED SKIN");
-    }
     const dest = path.join(dir, "dest");
-    const newFolder = path.join(CONST.Lrs_Root, CONST.Lrs_Room, "assets/UI", `skin_${name}`);
     const icon$pre = [`vs_${name}.png`, `vs_${name}_pre.png`];
     // 非图集资源
-    let notSheetAsset = [src].concat(icon$pre.map(e => path.join(dir, e)))      // icon 和 preview
-                        .push(path.join(dir, "**/*.exml"))                      // exml文件
-                        .push(path.join(dir, "**/*.jpg"));                      // jpg文件
+    let notSheetAsset = icon$pre.map(e => path.join(dir, "**", e))                        // icon 和 preview
+                                .concat([path.join(dir, "**/*.exml")])                    // exml文件
+                                .concat([path.join(dir, "**/*.jpg")]);                    // jpg文件
 
     // 需打入图集的资源
-    let sheetAsset = notSheetAsset.map(e => `!${e}`).push(path.join(dir, "**/*"));
-    let jpgs = [];
-
-    let processAsset = series(
+    let sheetAsset = [path.join(dir, "**/*.*")].concat(notSheetAsset.map(e => `!${e}`));
+    console.log(sheetAsset);
+    
+    return merge(
         // 拷贝非图集资源
         gulp.src(notSheetAsset)
-            .pipe(gulp.dest(dest)),
-
+        .pipe(P.rename(path => path.dirname = ""))
+        .pipe(gulp.dest(dest)),
+    
         // 拷贝图集资源到临时文件夹
         gulp.src(sheetAsset)
-            .pipe(gulp.dest(path.join(dest, "sheet"))),
-            
-        // 打包图集
-        gulp.src(path.join(dest, "sheet"))
-            .pipe(sheet(dest, `animSkin_${name}.json`))
-            .pipe(P.clean({force: true})),
+            .pipe(P.rename(path => path.dirname = ""))
+            .pipe(gulp.dest(path.join(dest, "sheet")))
+    )
+    .pipe(P.errorHandle());
+});
 
+gulp.task("_skin:sheet", () => {
+    const dir = args.dir;
+    const name = args.name;
+    const dest = path.join(dir, "dest");
+    // 打包图集
+    return gulp.src(path.join(dest, "sheet"), {allowEmpty: true})
+                .pipe(sheet(dest, `animSkin_${name}.json`))
+                .pipe(P.clean({force: true}))
+                .pipe(P.errorHandle());
+});
+
+gulp.task("_skin:process_src", () => {
+    const dir = args.dir;
+    const name = args.name;
+    const dest = path.join(dir, "dest");
+    const icon$pre = [`vs_${name}.png`, `vs_${name}_pre.png`];
+    const newFolder = path.join(CONST.Lrs_Root, CONST.Lrs_Room, "assets/UI", `skin_${name}`);
+
+    return merge(
         // 拷贝exml文件并注入lrsRoom.res.json
         gulp.src(path.join(dest, "**/*.exml"))
-            .pipe(gulp.dest(CONST.Lrs_Root, CONST.Lrs_Room, "assets/viproomskins"))
+            .pipe(gulp.dest(path.join(CONST.Lrs_Root, CONST.Lrs_Room, "assets/viproomskins")))
             .pipe(editResJson(lrsResJson, (file, groups, resources) => {
                 let fn = path.basename(file.path);
                 let fk = fn.replace(".exml", "_exml");
@@ -147,11 +155,7 @@ gulp.task("skin:add", () => {
                     "name": fk
                 });
             })),
-        gulp.src(path.join(dest, "*"))
-            .pipe(P.filterEach((_, fp) => {
-                let fn = path.basename(fp);
-                return fn.includes(`animSkin_${name}`);
-            }))
+        gulp.src(path.join(dest, "**", `animSkin_${name}.*`))
             .pipe(compress())
             .pipe(gulp.dest(newFolder))
             .pipe(editResJson(lrsResJson, (file, groups, resources) => {
@@ -172,31 +176,62 @@ gulp.task("skin:add", () => {
                     "name": fk,
                     "subkeys": subkeys 
                 });
-            })),
-
-        // 注入皮肤配置文件vipSkin.json
-        gulp.src(path.join(CONST.Lrs_Root, CONST.Lrs_Room, "config/vipSkin.json"))
-            .pipe(je(json => {
-                for (let item of json) {
-                    if (item.id == id) {
-                        item.icon = `vs_${name}_png`;
-                        item.preview = `vs_${name}_pre_png`;
-                        item.anim = `dayNightAnim_${name}`;
-                        item.res = [`animSkin_${name}_json`].concat(jpgs.map(e => e.replace(".jpg", "_jpg"))).join(",");
-                        break;
-                    }
-                }
-                return json;
-            })),
-
-        // 删除中途生成的文件夹
-        gulp.src(dest)
-            .pipe(P.clean({force: true}))
-    );
-
-    // src
-    //   -dest
-    return processAsset
-            .pipe(P.errorHandle());
-
+            }))  
+    )
+    .pipe(P.errorHandle());
 });
+
+let jpgs = [];
+gulp.task("_skin:gen_config", () => {
+    const dir = args.dir;
+    const name = args.name;
+    const id = args.id;
+    // 注入皮肤配置文件vipSkin.json
+    return gulp.src(path.join(CONST.Lrs_Root, CONST.Lrs_Room, "config/vipSkin.json"))
+                .pipe(je(json => {
+                    for (let item of json) {
+                        if (item.id == id) {
+                            item.icon = `vs_${name}_png`;
+                            item.preview = `vs_${name}_pre_png`;
+                            item.anim = `dayNightAnim_${name}`;
+                            item.res = [`animSkin_${name}_json`].concat(jpgs.map(e => e.replace(".jpg", "_jpg"))).join(",");
+                            break;
+                        }
+                    }
+                    return json;
+                }))
+                .pipe(stringify())
+                .pipe(P.beautify({
+                    keep_array_indentation: true,
+                    brace_style: "expand",
+                    indent_size: 4,
+                    indent_char: " "
+                }))
+                .pipe(gulp.dest(path.join(CONST.Lrs_Root, CONST.Lrs_Room, "config")))
+                .pipe(P.errorHandle());
+});
+
+gulp.task("_skin:del_tmp_folder", () => {
+    const dir = args.dir;
+    const dest = path.join(dir, "dest");
+    // 删除中途生成的文件夹
+    return gulp.src(dest, {allowEmpty: true})
+    .pipe(P.clean({force: true}))
+    .pipe(P.errorHandle());
+});
+
+gulp.task("skin:add", gulp.series(cb => {
+    let dir = args.dir;
+    let name = args.name;
+    let id = args.id;
+    if (!dir || typeof (dir) != "string") {
+        throw new PluginError("skin:added", "YOU MUST SPECIFY A DIRECTORY THAT CONTAINS ALL ASSETS SKIN NEEDED");
+    }
+    if (!name || typeof(name) != "string") {
+        throw new PluginError("skin:added", "YOU MUST SPECIFY A NAME AS NEW FOLDER PLACE SKIN ASSETS");
+    }
+    if (!id) {
+        throw new PluginError("skin:added", "YOU MUST SPECIFY A ID WHICH CONFIG THE ADDED SKIN");
+    }
+    cb();
+}, "_skin:move_src", "_skin:sheet", "_skin:process_src", "_skin:gen_config", "_skin:del_tmp_folder"));
